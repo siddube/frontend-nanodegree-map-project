9abdc8407d11fc65551d26296b178a9ce5b91fb5
Prajwal <prajwal@gmail.com>
2016-04-25 13:27:59 +0530

    Add Map Marker Click Functionality, Add infoWindow

--- js/app.js
+++ js/app.js
@@ -1,31 +1,39 @@
 var initialPlaces = [
     {
         'name' : 'Vidhana Soudha',
-        'latLng' : {'lat' : 12.97971,'lng' : 77.59079}
+        'latLng' : {'lat' : 12.97971,'lng' : 77.59079},
+        'description': 'The Vidhana Soudha located in Bengaluru (Bangalore), is the seat of the state legislature of Karnataka. It is an imposing building, constructed in a style sometimes described as Mysore Neo-Dravidian, and incorporates elements of Indo-Saracenic and Dravidian styles.The construction was completed in 1956.'
     },
     {
         'name' : 'Cubbon Park',
-        'latLng' : {'lat' : 12.97559,'lng' : 77.59268}
+        'latLng' : {'lat' : 12.97559,'lng' : 77.59268},
+        'description': 'Cubbon Park is a landmark \'lung\' area of the Bangalore city, located within the heart of city in the Central Administrative Area.'
     },
     {
         'name' : 'Visvesvaraya Museum',
-        'latLng' : {'lat' : 12.97527,'lng' : 77.5963}
+        'latLng' : {'lat' : 12.97527,'lng' : 77.5963},
+        'description': 'The Visvesvaraya Industrial and Technological Museum, Bangalore(VITM), India. a constituent unit of National Council of Science Museums (NCSM), Ministry of Culture, Government of India, was established in memory of Bharat Ratna Sir M Visvesvaraya.'
+        
     },
     {
-        'name' : 'Jawaharlal Nehru Planetarium',
-        'latLng' : {'lat' : 12.9848,'lng' : 77.589266}
+        'name' : 'Nehru Planetarium',
+        'latLng' : {'lat' : 12.9848,'lng' : 77.589266},
+        'description': 'Nehru Planetarium is the name given to 5 planetariums in India, named after India\'s first Prime Minister, Jawaharlal Nehru. One of them is situated in Bangalore'
     },
     {
         'name' : 'M. Chinnaswamy Stadium',
-        'latLng' : {'lat' : 12.97866,'lng' : 77.59975}
+        'latLng' : {'lat' : 12.97866,'lng' : 77.59975},
+        'description': 'The M. Chinnaswamy Stadium, located in Bangalore, Karnataka, is one of the cricket stadiums of India. Flanked by the picturesque Cubbon Park, Queen\'s Road, Cubbon and uptown MG Road, this four decade old stadium is situated in the heart of the city of Bangalore.'
     }
 ];
 
-
+var wikiapi;
+var $wikiElem;
 
 var Place = function(data) {
     this.name = ko.observable(data.name);
-    this.latLng = ko.observable(data.latLng); 
+    this.latLng = ko.observable(data.latLng);
+    this.description = ko.observable(data.description);
 };
 
 var ViewModel = function() {
@@ -44,19 +52,19 @@ var ViewModel = function() {
     initialPlaces.forEach(function(place) {
         self.visiblePlaces.push(place);
     });
-    var counter = 0;
+    var filterCounter = 0;
     
     self.filterPlaces = ko.computed(function() {
         //self.visiblePlaces.removeAll();
         
         return ko.utils.arrayFilter(self.placeList(), function(places){
-            counter++;
+            filterCounter++;
             var result = places.name().toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
-            if (result && counter > initialPlaces.length) { 
+            if (result && filterCounter > initialPlaces.length) { 
                self.visiblePlaces.push(places)
                places.marker.setVisible(true);
             }
-            else if (!result && counter > initialPlaces.length) {
+            else if (!result && filterCounter > initialPlaces.length) {
                 places.marker.setVisible(false);//.setVisible(false)
             }
             return result;
@@ -65,14 +73,16 @@ var ViewModel = function() {
     
     self.placeClick = function(clickedPlace){
         if (this.name) {
+            infoWindow.setPosition(clickedPlace.marker.position);
+            infoWindow.setContent('<p class="lead">'+clickedPlace.name()+'</p><br><p>'+clickedPlace.description()+'</p>');
+            infoWindow.open(map, marker);
            for(var i = 0; i< self.placeList().length; i++){
                 self.placeList()[i].marker.setAnimation(null);
             }
             map.panTo(clickedPlace.latLng());
             clickedPlace.marker.setAnimation(google.maps.Animation.BOUNCE);
-            console.log(this.name);
-            var wikiapi = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+ this.name();
-            var $wikiElem = $('#wiki-panel');
+            wikiapi = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+ this.name();
+            $wikiElem = $('#wiki-panel');
             $wikiElem.html('');
             $.ajax({
                 'url': wikiapi,
@@ -85,6 +95,31 @@ var ViewModel = function() {
             });
         }
     };
+    
+    self.markerClick = function(c) {
+        
+        infoWindow.setPosition(c.position);
+        infoWindow.open(map, marker);
+        self.placeList().forEach(function(place) {
+            place.marker.setAnimation(null);
+            if(place.marker.position === c.position) {
+                infoWindow.setContent('<p class="lead">'+place.name()+'</p><br><p>'+place.description()+'</p>');
+                place.marker.setAnimation(google.maps.Animation.BOUNCE);
+                wikiapi = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+ place.name();
+                $wikiElem = $('#wiki-panel');
+                $wikiElem.html('');
+                $.ajax({
+                    'url': wikiapi,
+                    'dataType': 'jsonp',
+                    'success': function(data) {
+                        $.each(data[1], function(i, item) {
+                            $wikiElem.append('<p><a href="https://en.wikipedia.org/wiki/'+item+'"target="blank" >'+item+'</a></p>');
+                        });
+                    }
+                });
+            }
+        });
+    }
 };
 
 var myViewM = new ViewModel();
--- js/main.js
+++ js/main.js
@@ -6,29 +6,29 @@ $(document).ready(function(){
             $('#map').parent().removeClass('col-md-10');
             $('#map').parent().addClass('col-md-12');
             $('#wiki-wrapper').parent().removeClass('col-md-offset-2');
-            $('#wiki-wrapper').parent().removeClass('col-md-5');
-            $('#wiki-wrapper').parent().addClass('col-md-6');
-            $('#places-wrapper').parent().removeClass('col-md-5');
-            $('#places-wrapper').parent().addClass('col-md-6');
+            $('#wiki-wrapper').parent().removeClass('col-md-10');
+            $('#wiki-wrapper').parent().addClass('col-md-12');
         }
         else {
             $('.sidebar-wrapper').css('left','0').css('height','auto').css('min-height','60vh');
             $('#map').parent().removeClass('col-md-12');
             $('#map').parent().addClass('col-md-10');
             $('#wiki-wrapper').parent().addClass('col-md-offset-2');
-            $('#wiki-wrapper').parent().removeClass('col-md-6');
-            $('#wiki-wrapper').parent().addClass('col-md-5');
-            $('#places-wrapper').parent().removeClass('col-md-6');
-            $('#places-wrapper').parent().addClass('col-md-5');
+            $('#wiki-wrapper').parent().removeClass('col-md-12');
+            $('#wiki-wrapper').parent().addClass('col-md-10');
         }
     });
     if(screen.width < 768) {
         $('.sidebar-wrapper').addClass('in')
         $('.sidebar-wrapper').css('left','-100%').css('height','0');
     }
+    
+    
 });
 var map;
 var marker;
+var infoWindow;
+var infoWindowContent;
 function initMap() {
     // Create a map object and specify the DOM element for display.
     map = new google.maps.Map(document.getElementById('map'), {
@@ -41,8 +41,13 @@ function initMap() {
             map: map,
             position: myLatLng
         };
-    
-    myViewM.placeList()[i].marker = new google.maps.Marker(markerOptions);
+        myViewM.placeList()[i].marker = new google.maps.Marker(markerOptions);
+        myViewM.placeList()[i].marker.addListener('click', function(e){
+            myViewM.markerClick(this);
+        });
     }
+    infoWindow = new google.maps.InfoWindow({
+        content: infoWindowContent
+    });
 }
 
